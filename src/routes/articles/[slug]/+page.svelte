943f21553f79107229cf4e44b6c0cc13eca568fd
Raghav Yadav <isitraghav@gmail.com>
2024-11-30 20:47:52 +0530

    added timestamp and marked support

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -3,19 +3,20 @@
 	import { marked } from 'marked';
 	import { onMount } from 'svelte';
 	import axios from 'axios';
-	import parseMD from 'parse-md';
 
 	import { page } from '$app/stores';
 
 	let sanitized = '';
 	let title = '';
 	let description = '';
+	let data_published;
 	onMount(() => {
 		axios.get(`https://dev.to/api/articles/isitraghav/${$page.params.slug}`).then((res) => {
 			const article = res.data;
 			title = article.title;
 			description = article.description;
 			sanitized = article.body_html;
+			data_published = article.published_at;
 		});
 	});
 </script>
@@ -26,5 +27,12 @@
 </svelte:head>
 <div>
 	<h1 class="text-3xl">{title}</h1>
+	<h5 class="text-stone-600">
+		Published {new Date(data_published).toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric'
+		})}
+	</h5>
 </div>
-{@html sanitized}
+{@html insane(marked(sanitized))}
